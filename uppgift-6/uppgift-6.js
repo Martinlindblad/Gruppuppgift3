6b709de52d5c642ad927158348055c237d6801c1
Martinlindblad <martin.lindblad1>
2018-11-02 16:35:52 +0100

    Last commit

--- uppgift-6/uppgift-6.js
+++ uppgift-6/uppgift-6.js
@@ -1,10 +1,11 @@
-function factorial(tal){                // Funktion som tar in ett värde
-    if (tal === 1){
+function factorial(tal){                // Funktion som tar in ett värde från användaren och retrunerar 
+    if (tal === 0){
         return 1;
-    }
-    else{
-        return (tal * factorial(--tal));
+    }else if(tal > 170){
+        return "JS bug! Inmatning över 170 blir Infinity"; // Snabb lösning för Infinity
+    } else{
+        return tal * factorial(--tal);   
     }
 }
-let tal = prompt("Ange ett tal mellan 1-1000: ")
+let tal = prompt("Ange ett heltal mellan 1-1000: ")
 document.write(factorial(tal))
